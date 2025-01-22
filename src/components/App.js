85681974f98e2e91a8d8874c05f4e0fb02428177
Yuvraj Singh <yuvrajisking66@gmail.com>
2025-01-22 21:56:47 +0530

    commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,15 @@
 
-import React from "react";
+import React, { useState } from "react";
 import './../styles/App.css';
 
 const App = () => {
+
+  const[count,setcount]=useState(0)
   return (
-    <div>
-        {/* Do not remove the main div */}
+    <div style={{display:"flex"}}>
+        Button clicked {count} times
+
+        <button onClick={()=>setcount(count+1)}>Click me</button>
     </div>
   )
 }
